fc6d9bf2d35c71a2c34ec39f62aa2c7e4db43071
AlvinSet <alvingm17@gmail.com>
2024-06-24 10:13:49 -0300

    add login

--- aplicaciones-hibridas-parcial-2/src/App.jsx
+++ aplicaciones-hibridas-parcial-2/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Home from "./pages/Home";
 import {BrowserRouter, Routes,  Route} from "react-router-dom";
+import Login from "./pages/Login";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -14,6 +15,8 @@ function App() {
 
     <Routes>
     <Route path='/' element={<Home/>} />
+
+    <Route path="/login" element={<Login />} />
     </Routes>
 
     </BrowserRouter>
--- aplicaciones-hibridas-parcial-2/src/pages/Login.jsx
+++ aplicaciones-hibridas-parcial-2/src/pages/Login.jsx
@@ -1,6 +1,58 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Input, Button, Card, CardBody} from '@nextui-org/react';
 
+const Login = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const Home = () => {
+    const handleLogin = async () => {
+        const response = await fetch('http://localhost:3000/apiHotel/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            navigate('/');
+        } else {
+            alert(data.message);
+        }
+    };
 
-    // return()
-}
+    return (
+        <Card css={{ mw: "400px", margin: "auto" }}>
+            <h1>Login</h1>
+            <CardBody>
+                <Input
+                    clearable
+                    bordered
+                    fullWidth
+                    color="primary"
+                    size="lg"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    clearable
+                    bordered
+                    fullWidth
+                    color="primary"
+                    size="lg"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button fullWidth color="primary" auto onClick={handleLogin} css={{ marginTop: '20px' }}>
+                    Login
+                </Button>
+            </CardBody>
+        </Card>
+    );
+};
+
+export default Login;
